4e6d37b3cc19e4886239b22126ad1a29f7010a19
leedonghu <ldh930517@gmail.com>
2021-11-21 20:56:10 +0900

    first

---
+++ src/test/java/com/project/VueFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
